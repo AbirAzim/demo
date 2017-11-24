3ba9f588e202906e9c764b48f6f2230d0527aae1
badhon <badhonkhanbk007@gmail.com>
2017-11-24 12:42:06 +0600

    added a functionality

--- Test.java
+++ Test.java
@@ -1,7 +1,11 @@
 public class Test {
     public static void main(String[]args){
-        
-        System.out.println("I am bk");
 
+        System.out.println("I am bk");
+        greetings("bk");
+    }
+
+    public static void greetings(String name){
+        System.out.println("hello"+name)
     }
 }
